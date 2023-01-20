2887a319a3668b683c88c3ebeb96fe1928396462
Caspian2 <casa04001@utb.vaxjo.se>
2023-01-20 10:23:09 +0100

    Add project files.

---
+++ uppgift 5.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv antalet meningar du vill skriva ");
+            int meningar = int.Parse(Console.ReadLine());
+            string[] mening = new string[meningar];
+
+
+            for(int i=0; i < meningar; i++)
+            {
+                Console.WriteLine("Skriv mening");
+                mening[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Du har skrivit meningar:");
+            Console.Write("");
+
+            for (int i=0; i < meningar; i++)
+            {
+                Console.Write($" \n{mening[i]}");
+            }
+
+
+        }
+    }
+}
